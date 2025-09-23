d663e33cfce81f062a1098a6f8fd44d3c0f37df1
BogovarovaSasha <abogovarova@gmail.com>
2025-09-24 00:42:07 +0500

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
+using namespace std;
 void boolis35(int x);
 
-int main() {
-    int a;
-    std::cout << "Введите число" ;
-    std::cin >> a;
-    boolis35(a);
-    return 0;
-}
 void boolis35(int x) {
     if ( x % 3 == 0 && x % 5 == 0) {
         std::cout << "False"<< std::endl;
@@ -18,4 +12,18 @@ void boolis35(int x) {
     else {
         std::cout << "False"<< std::endl;
     }
+}
+
+int main() {
+    int a;
+    
+    cout << "Введите число: ";
+    while (!(cin >> a)) {
+        cout << "Ошибка! Введите число: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+    
+    boolis35(a);
+    return 0;
 }
