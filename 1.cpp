ac653d002c5c01981cc04116b653865b493740da
BogovarovaSasha <abogovarova@gmail.com>
2025-09-24 00:23:22 +0500

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
-int charToNum (char x) {
-    return x - '0';
+
+int charToNum(char x) {
+    return x - '0'; 
 }
+
 int main() {
-std::cout << charToNum('0') << std::endl;
-std::cout << charToNum('1') << std::endl;
-std::cout << charToNum('2') << std::endl;
-std::cout << charToNum('3') << std::endl;
-std::cout << charToNum('4') << std::endl;
-std::cout << charToNum('5') << std::endl;
-std::cout << charToNum('6') << std::endl;
-std::cout << charToNum('7') << std::endl;
-std::cout << charToNum('8') << std::endl;
-std::cout << charToNum('9') << std::endl;
-return 0;
+    char a; 
+    std::cout << "Введите число: ";
+    std::cin >> a;
+    
+    if (a >= '1' && a <= '9') {
+        int result = charToNum(a);
+        std::cout << "Результат: " << result << std::endl;
+    } else {
+        std::cout << "Некорректный ввод" << std::endl;
+    }
+    
+    return 0;
 }
